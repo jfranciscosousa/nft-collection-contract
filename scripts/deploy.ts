14f6295eea09d390a24ff0ffe00af1ee111f0416
Francisco Sousa <francisco.sousa@hey.com>
2021-09-15 01:34:21 +0100

    Improve deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,7 @@
 import { parseEther } from "@ethersproject/units";
 import hre from "hardhat";
 import { MyToken, MyToken__factory } from "../typechain";
+import dateToBlockchain from "../utils/dateToBlockchain";
 
 const { MAX_TOKENS, BASE_URI, MINT_PRICE, LAUNCH_DATE } = process.env;
 
@@ -8,23 +9,34 @@ async function main() {
   const MyToken: MyToken__factory = (await hre.ethers.getContractFactory(
     "MyToken"
   )) as MyToken__factory;
+  console.log("deploying the MyToken contract");
+
   const myToken: MyToken = (await MyToken.deploy(
     MAX_TOKENS as string,
     BASE_URI as string,
     parseEther(MINT_PRICE as string),
-    new Date(LAUNCH_DATE as string).getTime() / 1000
+    dateToBlockchain(new Date(LAUNCH_DATE as string))
   )) as MyToken;
 
-  await myToken.deployed();
+  await myToken.deployTransaction.wait(6);
+
+  console.log("token contract deployed to:", myToken.address);
 
   if (hre.network.name !== "hardhat" && hre.network.name !== "localhost") {
+    console.log("verifying the contract on etherscan");
+
     await hre.run("verify:verify", {
       address: myToken.address,
-      constructorArguments: [],
+      constructorArguments: [
+        MAX_TOKENS,
+        BASE_URI,
+        parseEther(MINT_PRICE as string),
+        dateToBlockchain(new Date(LAUNCH_DATE as string)),
+      ],
     });
   }
 
-  console.log("MyToken deployed to:", myToken.address);
+  console.log("all done!");
 }
 
 main()
